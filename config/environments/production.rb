ee3ae461a2bcde88f33a158471d37ab126415779
Aubrey Holland <aubreyholland@gmail.com>
2016-08-31 18:21:45 -0400

    mailgun yo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "dhsflash_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: ENV['EMAIL_DEFAULT_HOST'] }
+  ActionMailer::Base.smtp_settings = {
+    port: ENV['MAILGUN_SMTP_PORT'],
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    domain: 'app61da1addd0714389aec9c5efc78b553c.mailgun.org',
+    authentication: :plain
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
