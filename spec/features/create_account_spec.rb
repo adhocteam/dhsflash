0b00060a441d133afcdc6925decba18c90bedb49
Aubrey Holland <aubreyholland@gmail.com>
2016-08-31 14:02:00 -0400

    more tests

--- spec/features/create_account_spec.rb
+++ spec/features/create_account_spec.rb
@@ -2,26 +2,31 @@ require 'rails_helper'
 
 describe 'the signup process', type: :feature do
   context 'with the new account form filled out' do
+    let(:email) { Faker::Internet.email }
+
     before do
       visit '/users/sign_up'
       within('form') do
-        fill_in 'Email', with: Faker::Internet.email
+        fill_in 'Email', with: email
         fill_in 'Username', with: Faker::Internet.user_name
         fill_in 'First name', with: Faker::Name.first_name
         fill_in 'Last name', with: Faker::Name.last_name
-        fill_in 'Password', with: 'abc-123'
+        find('#user_password').set('abc-123')
         fill_in 'Password confirmation', with: 'abc-123'
-        click_button 'Log in'
+        click_button 'Sign up'
       end
     end
 
-    it 'has signed me in' do
-      expect(page).to have_content 'Signed in successfully'
-      expect(page).to have_content 'Log Out'
+    it 'has created an account' do
+      user = User.where(email: email).first
+      expect(user).to be
+      expect(user.confirmed_at).to be_nil
+      expect(page).to have_content 'A message with a confirmation link has been sent to your email address'
+      expect(page).to have_content 'Log In'
     end
 
     it 'takes me to the dashboard' do
-      expect(current_path).to eq(dashboard_path)
+      expect(current_path).to eq(root_path)
     end
   end
 end
