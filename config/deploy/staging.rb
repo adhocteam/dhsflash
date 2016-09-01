1295a7a99ba3633040cce353acddfcfd9f4786ef
Chris Gansen <cgansen@gmail.com>
2016-09-01 18:56:06 -0400

    setting up staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-server 'ec2-54-162-68-27.compute-1.amazonaws.com', user: 'ec2-user', roles: %w(app db web)
+server 'ec2-54-196-181-31.compute-1.amazonaws.com', user: 'ec2-user', roles: %w(app db web)
 set :ssh_options,
     keys: %w(/Users/cgansen/.ssh/dhsflash.pem),
     forward_agent: true
