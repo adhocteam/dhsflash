fea6fa6ea960c8f0c87376c50dd101866e4eeac9
Aubrey Holland <aubreyholland@gmail.com>
2016-09-01 15:57:24 -0400

    more gravatar fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,9 +43,4 @@ class User < ApplicationRecord
       false
     end
   end
-
-  def avatar_url
-    gravatar_id = Digest::MD5.hexdigest(email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=150"
-  end
 end
