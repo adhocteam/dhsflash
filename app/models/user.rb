35ef9c3bc5e9c5dd7c88069ad0623e6a29fb21af
Aubrey Holland <aubreyholland@gmail.com>
2016-09-01 09:04:57 -0400

    Notifications (#27)
    * add email notifications for new kudos
    
    * default notification stuff
    
    * send thumbs up emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,16 @@ class User < ApplicationRecord
   def name
     "#{first_name} #{last_name}"
   end
+
+  def should_get_notification?(type)
+    if notification_frequency == 'none'
+      false
+    elsif notification_frequency == 'all'
+      true
+    elsif notification_frequency == 'mentions' && type == :new_kudo
+      true
+    else
+      false
+    end
+  end
 end
