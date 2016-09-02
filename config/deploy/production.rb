c2d94d0113d650b6172b55c3d35f887ddb14879a
Chris Gansen <cgansen@gmail.com>
2016-09-02 10:51:02 -0400

    Update location of ec2 priv key
    Change-Id: Ie11a918eed01cd19c8b0df7f9a7a1b99e70a0ecd

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 server 'ec2-54-162-68-27.compute-1.amazonaws.com', user: 'ec2-user', roles: %w(app db web)
 set :ssh_options,
-    keys: %w(/Users/cgansen/.ssh/dhsflash.pem),
+    keys: %w(/var/lib/jenkins/.ssh/dhsflash.pem),
     forward_agent: true
